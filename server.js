3c56f3ffdfe457d98ea475453225e8828cc021be
lusifer <8083ankit@gmail.com>
2022-01-27 17:59:59 +0530

    Final Commit

--- server.js
+++ server.js
@@ -10,12 +10,18 @@ const PORT = process.env.PORT || 5000;
 //connect db
 connectDB();
 
-app.get("/", (req, res) => {
-    res.send("API RUNNING ;)");
-});
-
 app.use("/api/auth", require("./routes/api/auth"));
 app.use("/api/user", require("./routes/api/user"));
+
+//Serve static assets in production
+if (process.env.NODE_ENV === "production") {
+    //Set static folder
+    app.use(express.static("client/build"));
+
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+    });
+}
 
 app.listen(PORT, () => {
     console.log(`Server up and running at port ${PORT}`);
